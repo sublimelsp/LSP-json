4a3942bff868532378929b2404ca6637649e280f
Predrag <idmpepe@gmail.com>
2019-07-29 00:25:32 +0200

    Use sublime.packages_path()

--- plugin.py
+++ plugin.py
@@ -1,39 +1,63 @@
 import shutil
 import os
 import sublime
+import threading
+import subprocess
 
 from LSP.plugin.core.handlers import LanguageHandler
 from LSP.plugin.core.settings import ClientConfig, LanguageConfig
 from .schemas import schemas
 
 
-package_path = os.path.dirname(__file__)
-server_path = os.path.join(package_path, 'node_modules', 'vscode-json-languageserver-bin', 'jsonServerMain.js')
-
-
 def plugin_loaded():
+    package_path = os.path.join(sublime.packages_path(), 'LSP-json')
+    server_path = os.path.join(package_path, 'node_modules', 'vscode-json-languageserver-bin', 'jsonServerMain.js')
     print('LSP-json: Server {}.'.format('installed' if os.path.isfile(server_path) else 'is not installed' ))
 
+    # install the node_modules if not installed
     if not os.path.isdir(os.path.join(package_path, 'node_modules')):
-        # install server if no node_modules
-        print('LSP-json: Installing server.')
-        sublime.active_window().run_command(
-            "exec", {
-                "cmd": [
-                    "npm",
-                    "install",
-                    "--verbose",
-                    "--prefix",
-                    package_path
-                ]
-            }
-        )
-        sublime.message_dialog('LSP-json\n\nRestart sublime after the server has been installed successfully.')
+        # this will be called only when the plugin gets:
+        # - installed for the first time,
+        # - or when updated on package control
+        logAndShowMessage('LSP-json: Installing server.')
 
+        runCommand(
+            onCommandDone,
+            ["npm", "install", "--verbose", "--prefix", package_path]
+        )
+
+
+def onCommandDone():
+    logAndShowMessage('LSP-json: Server installed.')
+
+
+def runCommand(onExit, popenArgs):
+    """
+    Runs the given args in a subprocess.Popen, and then calls the function
+    onExit when the subprocess completes.
+    onExit is a callable object, and popenArgs is a list/tuple of args that
+    would give to subprocess.Popen.
+    """
+    def runInThread(onExit, popenArgs):
+        try:
+            subprocess.check_call(popenArgs)
+            onExit()
+        except subprocess.CalledProcessError as error:
+            logAndShowMessage('LSP-json: Error while installing the server.', error)
+        return
+    thread = threading.Thread(target=runInThread, args=(onExit, popenArgs))
+    thread.start()
+    # returns immediately after the thread starts
+    return thread
 
 
 def is_node_installed():
     return shutil.which('node') is not None
+
+
+def logAndShowMessage(msg, additional_logs=None):
+    print(msg, '\n', additional_logs) if additional_logs else print(msg)
+    sublime.active_window().status_message(msg)
 
 
 class LspJSONPlugin(LanguageHandler):
@@ -43,6 +67,9 @@ class LspJSONPlugin(LanguageHandler):
 
     @property
     def config(self) -> ClientConfig:
+        package_path = os.path.join(sublime.packages_path(), 'LSP-json')
+        server_path = os.path.join(package_path, 'node_modules', 'vscode-json-languageserver-bin', 'jsonServerMain.js')
+
         return ClientConfig(
             name='lsp-json',
             binary_args=[
