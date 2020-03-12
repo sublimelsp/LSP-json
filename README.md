c65c011a0404e9f6f54ad7450fde33ed543cc865
Rafał Chłodnicki <rchl2k@gmail.com>
2020-03-12 22:33:11 +0100

    Update server and make it run from cache directory
     - update `vscode-json-languageserver` to version 1.2.3
     - update schemas.py with latest updates from http://schemastore.org/api/json/catalog.json
     - add palette command for opening LSP-json settings
     - run server from cache directory

--- README.md
+++ README.md
@@ -2,9 +2,11 @@
 
 JSON support for Sublime's LSP plugin.
 
+Uses [VSCode JSON Language Server](https://github.com/Microsoft/vscode/tree/master/extensions/json-language-features/server) to provide validation, formatting and other features for JSON files. See linked repository for more information.
+
 * Install [LSP](https://packagecontrol.io/packages/LSP) and `LSP-json` from Package Control.
 * Restart Sublime.
 
 ### Configuration
 
-Configure the json language server by accessing `Preferences > Package Settings > LSP > Servers > LSP-json`.
+Open configuration file using command palette with `Preferences: LSP-json Settings` command or opening it from the Sublime menu (`Preferences > Package Settings > LSP > Servers > LSP-json`).
--- plugin.py
+++ plugin.py
@@ -1,85 +1,50 @@
 import shutil
 import os
 import sublime
-import threading
-import subprocess
 
 from LSP.plugin.core.handlers import LanguageHandler
-from LSP.plugin.core.settings import ClientConfig, LanguageConfig, read_client_config
+from LSP.plugin.core.settings import ClientConfig, read_client_config
+from lsp_utils import ServerNpmResource
 from .schemas import schemas
 
-package_path = os.path.dirname(__file__)
-server_path = os.path.join(package_path, 'node_modules', 'vscode-json-languageserver', 'bin', 'vscode-json-languageserver')
+PACKAGE_NAME = 'LSP-json'
+SETTINGS_FILENAME = 'LSP-json.sublime-settings'
+SERVER_DIRECTORY = 'vscode-json-languageserver'
+SERVER_BINARY_PATH = os.path.join(SERVER_DIRECTORY, 'node_modules', 'vscode-json-languageserver',
+                                  'bin', 'vscode-json-languageserver')
+
+server = ServerNpmResource(PACKAGE_NAME, SERVER_DIRECTORY, SERVER_BINARY_PATH)
 
 
 def plugin_loaded():
-    is_server_installed = os.path.isfile(server_path)
-    print('LSP-json: Server {}.'.format('installed' if is_server_installed else 'is not installed' ))
-
-    # install the node_modules if not installed
-    if not is_server_installed:
-        # this will be called only when the plugin gets:
-        # - installed for the first time,
-        # - or when updated on package control
-        logAndShowMessage('LSP-json: Installing server.')
-
-        runCommand(
-            onCommandDone,
-            ["npm", "install", "--verbose", "--prefix", package_path, package_path]
-        )
+    server.setup()
 
 
-def onCommandDone():
-    logAndShowMessage('LSP-json: Server installed.')
-
-
-def runCommand(onExit, popenArgs):
-    """
-    Runs the given args in a subprocess.Popen, and then calls the function
-    onExit when the subprocess completes.
-    onExit is a callable object, and popenArgs is a list/tuple of args that
-    would give to subprocess.Popen.
-    """
-    def runInThread(onExit, popenArgs):
-        try:
-            if sublime.platform() == 'windows':
-                subprocess.check_call(popenArgs, shell=True)
-            else:
-                subprocess.check_call(popenArgs)
-            onExit()
-        except subprocess.CalledProcessError as error:
-            logAndShowMessage('LSP-json: Error while installing the server.', error)
-        return
-    thread = threading.Thread(target=runInThread, args=(onExit, popenArgs))
-    thread.start()
-    # returns immediately after the thread starts
-    return thread
+def plugin_unloaded():
+    server.cleanup()
 
 
 def is_node_installed():
     return shutil.which('node') is not None
 
 
-def logAndShowMessage(msg, additional_logs=None):
-    print(msg, '\n', additional_logs) if additional_logs else print(msg)
-    sublime.active_window().status_message(msg)
-
-
 class LspJSONPlugin(LanguageHandler):
     @property
     def name(self) -> str:
-        return 'lsp-json'
+        return PACKAGE_NAME.lower()
 
     @property
     def config(self) -> ClientConfig:
-        settings = sublime.load_settings("LSP-json.sublime-settings")
+        # Calling setup() also here as this might run before `plugin_loaded`.
+        # Will be a no-op if already ran.
+        # See https://github.com/sublimelsp/LSP/issues/899
+        server.setup()
+
+        settings = sublime.load_settings(SETTINGS_FILENAME)
         client_configuration = settings.get('client')
         default_configuration = {
-            "command": [
-                'node',
-                server_path,
-                '--stdio'
-            ],
+            "enabled": True,
+            "command": ['node', server.binary_path, '--stdio'],
             "languages": [
                 {
                     "languageId": "json",
@@ -93,7 +58,8 @@ class LspJSONPlugin(LanguageHandler):
         }
         default_configuration.update(client_configuration)
         default_configuration['settings']['json']['schemas'] = schemas
-        return read_client_config('lsp-json', default_configuration)
+
+        return read_client_config(self.name, default_configuration)
 
     def on_start(self, window) -> bool:
         if not is_node_installed():
