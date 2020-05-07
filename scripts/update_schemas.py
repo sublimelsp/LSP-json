089bac1042559986e112935ced373b8ba5e76c59
Rafał Chłodnicki <rchl2k@gmail.com>
2020-05-07 22:28:05 +0200

    Filter out yaml entries

--- scripts/update_schemas.py
+++ scripts/update_schemas.py
@@ -1,21 +1,25 @@
 #!/usr/bin/env python3
 
 import os
+import re
 import requests
 
 from json import dumps
 
 DIRECTORY = os.path.dirname(__file__)
+RE_YAML = re.compile(r'\.ya?ml$')
 
 
 def main():
-    data = requests.get('http://schemastore.org/api/json/catalog.json').json()
-
-    # Pick only required properties
-    schema_list = [
-        {'fileMatch': schema['fileMatch'], 'url': schema['url']}
-        for schema in data['schemas'] if 'fileMatch' in schema
-    ]
+    schemas = requests.get('http://schemastore.org/api/json/catalog.json').json()['schemas']
+    schema_list = []
+    for schema in schemas:
+        fileMatch = schema.get('fileMatch')
+        url = schema['url']
+        if fileMatch:
+            fileMatch = list(filter(lambda pattern: not RE_YAML.search(pattern), schema['fileMatch']))
+        if fileMatch:
+            schema_list.append({'fileMatch': fileMatch, 'url': url})
 
     with open(os.path.join(DIRECTORY, '..', 'schemas.json'), 'w') as f:
         f.write(dumps(schema_list, indent=2))
