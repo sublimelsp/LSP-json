d8478919a7fc65a377ee8aed7b353153d6efcd5c
Predrag <idmpepe@gmail.com>
2021-04-03 23:12:56 +0200

    Include schemas that only have uri
    Those schemas were skiped, beacuse thay don't have fileMatch. But we need those.
    
    If we want to use the 2019-09 (ex draft-8) of the spec.
    Previously we didn't included it because it didn't had a fileMatch.
    
    now we can assign the 2019-09 draft to a schema with "$schema" keyword.
    
    "$schema": "https://json-schema.org/draft/2019-09/schema"

--- scripts/update_schemas.py
+++ scripts/update_schemas.py
@@ -22,11 +22,13 @@ def main():
     for schema in schemas:
         fileMatch = schema.get('fileMatch')
         url = schema['url']
+        if not fileMatch:
+            schema_list.append({'uri': url})
         if fileMatch:
             fileMatch = list(filter(lambda pattern: not RE_YAML.search(pattern), schema['fileMatch']))
         if fileMatch:
             fileMatch = list(map(to_absolute_pattern, fileMatch))
-            schema_list.append({'fileMatch': fileMatch, 'uri': url})
+            schema_list.append({'fileMatch': fileMatch, 'uri': url})        
 
     with open(os.path.join(DIRECTORY, '..', 'lsp-json-schemas.json'), 'w') as f:
         f.write(dumps(schema_list, indent=2))
