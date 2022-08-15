13ca3c4a3cb66f270b3dfa7b8bd104780c16d0a6
Предраг Николић <idmpepe@gmail.com>
2022-08-15 20:29:14 +0200

    do not apply messages.json if in /Packages/**

--- scripts/update_schemas.py
+++ scripts/update_schemas.py
@@ -20,18 +20,24 @@ def main():
     schemas = requests.get('http://schemastore.org/api/json/catalog.json').json()['schemas']
     schema_list = []
     for schema in schemas:
-        fileMatch = schema.get('fileMatch')
+        file_match = schema.get('fileMatch')
         url = schema['url']
-        if not fileMatch:
+        if not file_match:
             schema_list.append({'uri': url})
-        if fileMatch:
-            fileMatch = list(filter(lambda pattern: not RE_YAML.search(pattern), schema['fileMatch']))
-        if fileMatch:
-            fileMatch = list(map(to_absolute_pattern, fileMatch))
-            schema_list.append({'fileMatch': fileMatch, 'uri': url})
+        if file_match:
+            file_match = list(filter(lambda pattern: not RE_YAML.search(pattern), schema['fileMatch']))
+        if file_match:
+            file_match = list(map(to_absolute_pattern, file_match))
+            file_match = fix_edge_cases(file_match)
+            schema_list.append({'fileMatch': file_match, 'uri': url})
 
     with open(os.path.join(DIRECTORY, '..', 'lsp-json-schemas.json'), 'w') as f:
         f.write(dumps(schema_list, indent=2))
+
+def fix_edge_cases(file_match):
+    if "/messages.json" in file_match:
+        return ["!/Packages/**/messages.json", *file_match]
+    return file_match
 
 
 if __name__ == '__main__':
